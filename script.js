f11f1a07cd1d656cec11532fbc538242867d4736
Al Mahmud <mdalmahmud0183@gmail.com>
2018-05-30 03:00:37 +0000

    Task2

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ console.log(heading.innerText)
 
 
 /* Task 2 using plain JavaScript */
+function addText2(){
+    var Task2div =document.getElementById('task2a')
+    var paragraph =document.createElement('p')
+    paragraph.innerText ="Hello word"
+    Task2div.appendChild(paragraph)
+}
+var textBtn =document.getElementById('textBtn2a')
+textBtn.addEventListener('click',addText2)
 
 
 
